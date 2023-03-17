d98915a6749d9ac9422303c733a2ddc85230719d
Juan Huerta <jmhuerta@DESKTOP-C6RMNAG>
2023-03-16 23:01:36 -0400

    removed some unused code

--- src/main.rs
+++ src/main.rs
@@ -9,14 +9,6 @@ use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
 
-#[allow(dead_code)]
-fn complex_square_add_loop(c: Complex<f64>) {
-    let mut z = Complex { re: 0.0, im: 0.0 };
-    loop {
-        z = z * z + c;
-    }
-}
-
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex { re: 0.0, im: 0.0 };
     for i in 0..limit {
